0026f299453fdda15d2a13c546fa6fcd2d5c40d2
lekhrajprasad <lekhraj.prasad@gmail.com>
2020-09-30 14:36:48 +0530

    deleted

--- mybookapp/MyBookSearchMS/src/main/java/com/coursecube/booksearch/dao/BookInventoryDAO.java
+++
@@ -1,7 +0,0 @@
-package com.coursecube.booksearch.dao;
-
-import com.coursecube.booksearch.entity.BookInventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookInventoryDAO extends JpaRepository<BookInventory, Integer> {
-}
--- mybookapp/MyBookSearchMS/src/main/java/com/coursecube/booksearch/dao/BookRatingDAO.java
+++
@@ -1,7 +0,0 @@
-package com.coursecube.booksearch.dao;
-
-import com.coursecube.booksearch.entity.BookRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRatingDAO extends JpaRepository<BookRating, Integer> {
-}
